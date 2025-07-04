1dddb608c614812cff78586aa52869e36bc97bfd
Vikrant Mainwal <vikrantmainwal1301@gmail.com>
2025-07-04 11:57:03 +0530

    closing note & liked heart

--- components/about/ClosingNote.js
+++ components/about/ClosingNote.js
@@ -1,8 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
+import mail from '../../assets/images/mail.png'
+import logo from '../../assets/images/logo/logo.svg'
 
 const ClosingNote = () => {
   return (
-    <div className='text-center mt-25 mb-25'>ClosingNote</div>
+    <div className='text-center mt-25 mb-25 max-w-[1440px] mx-auto h-[38rem]'>
+      <div className='relative flex justify-center '>
+      <div className='bg-[#C95D2F] w-[25rem] h-[25rem] rounded-full absolute top-5 left-24 z-0'/>
+      <div className='z-2 relative'>
+      <Image src={mail} alt='image' />
+      <div className=' absolute top-24 right-10 space-y-7'>
+        <div className='flex items-end justify-end'>
+        <Image src={logo} alt='image' />
+        </div>
+        <p className='text-[2.375rem] italic font-bold lora w-[20rem]'>“From our <span className='text-[#C95D2F]'>home</span> in the <span className='text-[#C95D2F]'>hills</span> to <span className='text-[#C95D2F]'>yours”</span></p>
+        <div className='flex flex-col justify-end items-end'>
+          <p className='text-[1rem] font-medium'>With Warmth,</p>
+          <p className='text-[#C95D2F] opacity-70 text-[1rem] font-medium'> The Triyuginarayan Mandap Team</p>
+          <p className='opacity-70 text-[1rem] font-medium'> (Companion to your divine wedding journey)</p>
+        </div>
+      </div>
+      </div>
+      <div className='bg-[#C95D2F] w-[20rem] h-[20rem] rounded-full absolute bottom-0 right-24 z-0'/>
+      </div>
+    </div>
   )
 }
 
--- components/why-choose/TriyuginarayanOverview.js
+++ components/why-choose/TriyuginarayanOverview.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import Image from "next/image";
 import rituals from "../../assets/images/rituals.png";
@@ -8,20 +8,18 @@ import triyuginarayan_temple from "../../assets/images/triyuginarayan-temple.png
 import choose_us from "../../assets/images/choose-us.png";
 
 const TriyuginarayanOverview = () => {
-   const [liked, setLiked] = useState(false);
+  const [liked, setLiked] = useState(false);
   const [animate, setAnimate] = useState(false);
 
   const handleClick = () => {
     setLiked(!liked);
     setAnimate(true);
-
-    // Remove animation class after animation completes (e.g., 300ms)
-    setTimeout(() => setAnimate(false), 300);
+    setTimeout(() => setAnimate(false), 300); // Reset duration
   };
 
   return (
-    <div>
-      <div className="max-w-[1440px] bg-[#C95D2F] h-[30rem] relative">
+    <div className="">
+      <div className="max-w-[1440px] bg-[#C95D2F] h-[30rem] relative m-auto">
         <div className="flex justify-center items-center h-full px-20">
           <p className="w-[70rem] text-[3.4375rem] text-white font-bold lora z-10 leading-tight">
             Why Choose <span className="text-[#F6E8C1]">Triyuginarayan</span>{" "}
@@ -40,93 +38,122 @@ const TriyuginarayanOverview = () => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-32 px-10 mt-25">
+      <div className="max-w-[1440px] m-auto flex flex-col gap-32 px-10 mt-25">
         <div className="flex">
-            <div className="flex flex-col w-[60%] p-10">
+          <div className="flex flex-col w-[60%] p-10">
             <div className="flex items-center justify-center gap-3">
               <span className="text-[6.25rem] font-semibold">1.</span>
               <p className="text-[2.25rem] font-bold">
-                The Place Where Lord Shiva <span className="text-[#C95D2F]">Married</span> Parvati.
+                The Place Where Lord Shiva{" "}
+                <span className="text-[#C95D2F]">Married</span> Parvati.
               </p>
-              </div> 
-              <p>Triyuginarayan is the same place where Lord Shiva and Goddess Parvati got married.The fire from their wedding — the Akhand Dhuni — still burns inside the temple. Couples from all over India come here to take their vows where the gods once stood.If you’re looking for a wedding that’s not just beautiful, but blessed, this is the right place.</p>
             </div>
-
-            <div className="w-[40%] flex justify-center items-center">
-              <Image src={triyuginarayan_temple} alt="image" className="w-[20rem]"/>
-            </div>
+            <p>
+              Triyuginarayan is the same place where Lord Shiva and Goddess
+              Parvati got married.The fire from their wedding — the Akhand Dhuni
+              — still burns inside the temple. Couples from all over India come
+              here to take their vows where the gods once stood.If you’re
+              looking for a wedding that’s not just beautiful, but blessed, this
+              is the right place.
+            </p>
           </div>
+
+          <div className="w-[40%] flex justify-center items-center">
+            <Image
+              src={triyuginarayan_temple}
+              alt="image"
+              className="w-[20rem]"
+            />
+          </div>
+        </div>
         <div className="flex">
           <div className="w-[40%] flex justify-center items-center">
-              <Image src={mountains} alt="image" className="w-[20rem]"/>
-            </div>
-            <div className="flex flex-col w-[60%] p-10">
+            <Image src={mountains} alt="image" className="w-[20rem]" />
+          </div>
+          <div className="flex flex-col w-[60%] p-10">
             <div className="flex items-center justify-center gap-3">
               <span className="text-[6.25rem] font-semibold">2.</span>
               <p className="text-[2.25rem] font-bold">
-                <span className="text-[#C95D2F]">Quiet</span> Mountains. <span className="text-[#C95D2F]">Pure</span> Air. <span className="text-[#C95D2F]">Natural </span>Blessings.
+                <span className="text-[#C95D2F]">Quiet</span> Mountains.{" "}
+                <span className="text-[#C95D2F]">Pure</span> Air.{" "}
+                <span className="text-[#C95D2F]">Natural </span>Blessings.
               </p>
-              </div> 
-              <p>This is not a crowded city wedding. Triyuginarayan is a peaceful village in the Himalayas, surrounded by nature. The air is fresh, the views are beautiful, and the atmosphere is calm. It’s the perfect place to begin your new life — with peace in your heart and blessings all around.</p>
             </div>
-
-            
+            <p>
+              This is not a crowded city wedding. Triyuginarayan is a peaceful
+              village in the Himalayas, surrounded by nature. The air is fresh,
+              the views are beautiful, and the atmosphere is calm. It’s the
+              perfect place to begin your new life — with peace in your heart
+              and blessings all around.
+            </p>
           </div>
+        </div>
         <div className="flex">
-            <div className="flex flex-col w-[60%] p-10">
+          <div className="flex flex-col w-[60%] p-10">
             <div className="flex items-center justify-center gap-3">
               <span className="text-[6.25rem] font-semibold">3.</span>
               <p className="text-[2.25rem] font-bold">
-                <span className="text-[#C95D2F]">Real</span> Rituals. <span className="text-[#C95D2F]">Deep</span> Meaning. <span className="text-[#C95D2F]">Lifelong</span> Memories.
+                <span className="text-[#C95D2F]">Real</span> Rituals.{" "}
+                <span className="text-[#C95D2F]">Deep</span> Meaning.{" "}
+                <span className="text-[#C95D2F]">Lifelong</span> Memories.
               </p>
-              </div> 
-              <p>Weddings here are not rushed. You sit near the eternal fire, guided by local priests.Mantras are chanted with care, and the whole ceremony feels real — not just for photos, but for life. From Garhwali wedding songs to simple traditional rituals, every moment feels meaningful.</p>
             </div>
-
-            <div className="w-[40%] flex justify-center items-center">
-              <Image src={rituals} alt="image" className="w-[20rem]"/>
-            </div>
+            <p>
+              Weddings here are not rushed. You sit near the eternal fire,
+              guided by local priests.Mantras are chanted with care, and the
+              whole ceremony feels real — not just for photos, but for life.
+              From Garhwali wedding songs to simple traditional rituals, every
+              moment feels meaningful.
+            </p>
           </div>
+
+          <div className="w-[40%] flex justify-center items-center">
+            <Image src={rituals} alt="image" className="w-[20rem]" />
+          </div>
+        </div>
         <div className="flex">
-           <div className="w-[40%] flex justify-center items-center">
-              <Image src={couple} alt="image" className="w-[20rem]"/>
-            </div>
-            <div className="flex flex-col w-[60%] p-10">
+          <div className="w-[40%] flex justify-center items-center">
+            <Image src={couple} alt="image" className="w-[20rem]" />
+          </div>
+          <div className="flex flex-col w-[60%] p-10">
             <div className="flex items-center justify-center gap-3">
               <span className="text-[6.25rem] font-semibold">4.</span>
               <p className="text-[2.25rem] font-bold">
-                A Wedding for the <span className="text-[#C95D2F]">Soul, Not the Show.</span>
+                A Wedding for the{" "}
+                <span className="text-[#C95D2F]">Soul, Not the Show.</span>
               </p>
-              </div> 
-              <p>Many couples today don’t want a big show, they want a wedding that feels true that’s why Triyuginarayan is becoming one of the most loved sacred Indian wedding destinations. No loud music, no stress — just your loved ones, your promises, and the blessings of Lord Shiva and Goddess Parvati.</p>
             </div>
-
-           
+            <p>
+              Many couples today don’t want a big show, they want a wedding that
+              feels true that’s why Triyuginarayan is becoming one of the most
+              loved sacred Indian wedding destinations. No loud music, no stress
+              — just your loved ones, your promises, and the blessings of Lord
+              Shiva and Goddess Parvati.
+            </p>
           </div>
+        </div>
       </div>
 
-       <div className="bg-[#fcf0c3] p-6 text-lg font-serif text-black text-center">
-      <p className="text-[3rem]">
-        “ If your{" "}
-        {/* <Image
-          src={liked ? "/heart-filled.svg" : "/heart-outline.svg"}
-          alt="heart"
-          className={`inline-block w-10 h-10 cursor-pointer align-middle transition-transform duration-300 ${
-            animate ? "scale-125" : "scale-100"
-          }`}
-          width={132}
-          height={132}
-          onClick={handleClick}
-        />{" "} */}
-        says <span className="text-[#c8552f] font-semibold">Yes</span> to{" "}
-        <span className="text-[#c8552f] font-semibold">
-          peace, tradition, and real blessings
-        </span>{" "}
-        — then{" "}
-        <span className="text-[#c8552f] font-semibold">Triyuginarayan</span> is
-        waiting for you.”
-      </p>
-    </div>
+      <div className="max-w-[1440px] m-auto text-lg font-serif text-black text-center mt-25 mb-25 px-26">
+        <p className="text-[3rem] font-bold lora">
+          “ If your
+          <Image
+            src={liked ? "/heart-filled.svg" : "/heart-outline.svg"}
+            alt="heart"
+            className={`inline-block w-22 h-22 cursor-pointer align-middle transition-transform duration-300 ease-in-out 
+    ${animate ? "scale-125 translate-x-4" : "scale-70 translate-x-0"}`}
+            width={132}
+            height={132}
+            onClick={handleClick}
+          />
+          says <span className="text-[#c8552f">Yes</span> to{" "}
+          <span className="text-[#c8552f]">
+            peace, tradition, and real blessings
+          </span>{" "}
+          — then <span className="text-[#c8552f]">Triyuginarayan</span> is
+          waiting for you.”
+        </p>
+      </div>
     </div>
   );
 };
